d472c23073c69ce75b5d44a7bd71bfead0d0c5aa
leeshko <pumpkin@tut.by>
2021-06-23 00:17:28 +0300

    feat: make swipes

--- src/carouselApp.js
+++ src/carouselApp.js
@@ -1,7 +1,6 @@
 'use strict';
 // import ReactDOM from 'react-dom'
 
-
 const pics = [
   'https://github.com/leeshko/react-game/blob/react-game/src/images/cards/0.png?raw=true',
   'https://github.com/leeshko/react-game/blob/react-game/src/images/cards/1.png?raw=true',
@@ -22,45 +21,77 @@ class CarouselApp extends React.Component {
   constructor(props) {
     super(props);
     this.imgNode = React.createRef();
+    this.miniPic = React.createRef();
     this.pressLeft = this.pressLeft.bind(this);
     this.pressRight = this.pressRight.bind(this);
-    this.startPosition = 0;
+    this.moveAllPics = this.moveAllPics.bind(this);
+    this.moveMiniPics = this.moveMiniPics.bind(this);
+    this.addLastTouchCoord = this.addLastTouchCoord.bind(this);
+    this.swipe = this.swipe.bind(this);
     this.state = {
-      scrollLeft: true,
-      scrollright: false
+      startPosition: 0,
+      swipeStartPoint: null,
+      swipeEndPoint: null
     };
   }
 
-
   pressLeft() {
-    if (this.startPosition >= pics.length * 300) {
-      console.log(pics.length * 300, this.startPosition)
-      this.startPosition -= 300;
-      const nodes = this.imgNode.current.childNodes;
-      nodes.forEach(element => {
-        element.style = `transform: translateX(${this.startPosition}px)`;
-      });
+    if (this.state.startPosition * -1 <= (pics.length - 2) * 400) {
+      this.setState({ startPosition: this.state.startPosition -= 400 });
+      this.moveAllPics(this.state.startPosition);
     }
-    return;
   }
 
   pressRight() {
-    if (this.startPosition !== 0) {
-
-      this.startPosition += 300;
-      const nodes = this.imgNode.current.childNodes;
-      nodes.forEach(element => {
-        element.style = `transform: translateX(${this.startPosition}px)`;
-      });
+    if (this.state.startPosition !== 0) {
+      this.setState({ startPosition: this.state.startPosition += 400 });
+      this.moveAllPics(this.state.startPosition);
     }
-    return;
   }
 
+  moveAllPics(position) {
+    const nodes = this.imgNode.current.childNodes;
+    nodes.forEach(element => {
+      element.style = `transform: translateX(${position}px)`;
+    });
+  }
+
+  moveMiniPics(e) {
+    this.miniPic.current.childNodes.forEach(e => e.firstChild.className = 'miniPic');
+    this.setState({ startPosition: -400 * (pics.indexOf(e.target.currentSrc)) });
+    this.miniPic.current.childNodes[pics.indexOf(e.target.currentSrc)].firstChild.className = 'miniPic active';
+  }
+
+  componentDidUpdate() {
+    this.moveAllPics(this.state.startPosition);
+    this.swipe();
+  }
+
+  addFirstTouchCoord(event) {
+    console.log(event.touches[0].clientX);
+    this.setState({ swipeStartPoint: event.touches[0].clientX });
+  }
+
+  addLastTouchCoord(event) {
+    this.setState({ swipeEndPoint: event.touches[0].clientX });
+  }
+
+  swipe() {
+    console.log(this.state)
+    if (this.state.swipeStartPoint + 50 < this.state.swipeEndPoint) {
+      this.pressRight();
+    }
+    if (this.state.swipeStartPoint - 50 > this.state.swipeEndPoint) {
+      this.pressLeft();
+    }
+  }
 
   render() {
     return (
-      <div className='slider' >
-
+      <div className='slider'
+        onTouchStart={(e) => this.addFirstTouchCoord(e)}
+        onTouchMove={(e) => this.addLastTouchCoord(e)}
+      >
         <div className='items' ref={this.imgNode}>
           {pics.map((element, index) => {
             return (
@@ -73,18 +104,32 @@ class CarouselApp extends React.Component {
 
         </div>
         <button className='btn prev'
-          onClick={this.pressLeft}>
+          onClick={this.pressRight}>
           left
         </button>
 
         <button className='btn next'
-          onClick={this.pressRight}>
+          onClick={this.pressLeft}>
           right
         </button>
+        <div className='preview'
+          ref={this.miniPic}
+          onClick={(e) => {
+            this.moveMiniPics(e);
+          }}>
+          {pics.map((e, index) => {
+            return (
+              <div key={index}>
+                <img className='miniPic' src={e}></img>
+              </div>
+            )
+          })}
+        </div>
       </div>
-
     )
   }
 }
+
+
 let domContainer = document.querySelector('#carousel');
 ReactDOM.render(<CarouselApp />, domContainer);
